dcd21730d55b97af9cfdaa25f455e22b5365044c
josephlw7 <vision4thefuture@outlook.com>
2025-07-20 08:46:06 -0700

    commit message

---
+++ sysinfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo" System Information Utility
+echo""
+
+echo"Welcome, select one of the following options using the number keys:
+1: Show System Info
+2: Show Disk Usage
+3: Show Current Users
+4: Exit
